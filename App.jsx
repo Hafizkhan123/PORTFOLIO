7b7bdde9ebaf21bc959ea33875e441e797c75959
HAFIZ KHAN <165005640+Hafizkhan123@users.noreply.github.com>
2025-01-30 22:43:56 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">
+          Hello, Tailwind CSS!
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
